f27f6ce27ee2ffb99edac95bbdb07de1153002ba
zhaoying <zhaoying@pjlab.org.cn>
2024-04-03 14:45:02 +0800

    update test_internlm url

--- libs/community/tests/integration_tests/llms/test_internlm.py
+++ libs/community/tests/integration_tests/llms/test_internlm.py
@@ -5,7 +5,7 @@ from langchain_community.llms.internlm import InternLM
 def test_lmdeploy_call() -> None:
     """ test valid call to internlm deployed by lmdeploy"""
     llm = InternLM(
-        endpoint_url="http://10.140.0.65:33434/v1/chat/completions",
+        endpoint_url="http://127.0.0.1:23333/v1/chat/completions",
         model="internlm2-chat-20b"
     )
     output = llm("hello",stop=["[UNUSED_TOKEN_145]"])
